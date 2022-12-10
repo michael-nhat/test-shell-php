5e5be29c86eeceec15d2f0aaecddc1bab756eea1
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-10 21:40:21 +0700

    run sh command

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,11 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
+use mikehaertl\shellcommand\Command;
 use \Psr\Http\Message\ResponseInterface as Response;
 require '../../vendor/autoload.php';
 
 require '../../src/utils/common-util.php';
 require '../../src/apis/read-file.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
@@ -15,12 +16,14 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 });
 
 $app->get('/readfile', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello, jlkdsfs");
+    $command = new Command('find /www/wwwroot/dede/ -name "*.php"');
+    if ($command->execute()) {
+        pe($command->getOutput());
+    } else {
+        pe($command->getError());
+        $exitCode = $command->getExitCode();
+    }
     return $response;
 });
-
-
-
-
 
 $app->run();
