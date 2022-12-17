2c007bc4d95580264bb4e21b15492bf9c4cb6bff
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-17 16:13:37 +0700

    improve

--- src/utils/common-util.php
+++ src/utils/common-util.php
@@ -19,3 +19,19 @@ function pe()
         echo '</pre>';
     }
 }
+
+function ped()
+{
+    $numargs = func_num_args();
+
+    $arg_list = func_get_args();
+    for ($i = 0; $i < $numargs; $i++) {
+        echo ' errr: <pre>';
+        echo 'iii: ';
+        echo $i;
+        echo 'valll: ';
+        var_dump($arg_list[$i]);
+        echo '</pre>';
+    }
+    die();
+}
