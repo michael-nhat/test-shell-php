4245dacb0d4d1b11f5071e163d643145332c31ca
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-17 13:58:05 +0700

    improve

--- src/apis/test.php
+++ src/apis/test.php
@@ -1,10 +1,11 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
 require '../vendor/autoload.php';
+require '../utils/common-util.php';
 
-
-function hello(Request $request, Response $response, array $args) {
+function hello(Request $request, Response $response, array $args)
+{
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
 
--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,4 @@
 <?php
-use mikehaertl\shellcommand\Command;
 use \Psr\Http\Message\ResponseInterface as Response;
 require '../../vendor/autoload.php';
 
@@ -7,23 +6,68 @@ require '../../src/utils/common-util.php';
 require '../../src/apis/read-file.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+function fileExists($fileName, $caseSensitive = true)
+{
 
-    return $response;
-});
-
-$app->get('/readfile', function (Request $request, Response $response, array $args) {
-    $command = new Command('find /www/wwwroot/dede/ -name "*.php"');
-    if ($command->execute()) {
-        pe($command->getOutput());
-    } else {
-        pe($command->getError());
-        $exitCode = $command->getExitCode();
+    if (file_exists($fileName)) {
+        return $fileName;
     }
-    return $response;
+    if ($caseSensitive) {
+        return false;
+    }
+
+    // Handle case insensitive requests
+    $directoryName = dirname($fileName);
+    $fileArray = glob($directoryName . '/*', GLOB_NOSORT);
+    $fileNameLowerCase = strtolower($fileName);
+    foreach ($fileArray as $file) {
+        if (strtolower($file) == $fileNameLowerCase) {
+            return $file;
+        }
+    }
+    return false;
+}
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response, array $args) {
+
+    try {
+        $fileUserInpPath = $request->getQueryParams()["filePath"];
+        $rawPath = "/www/wwwroot/php-for-shell/" . $fileUserInpPath;
+
+        // $path = "";
+        // if (file_exists($rawPath)) {
+        //     $path = $rawPath;
+        // }
+
+        // $test = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+        // echo $test;
+        // die();
+
+        $path = fileExists($rawPath, $caseSensitive = false);
+
+        if (!$path) {
+            throw new ErrorException("no file exist: " . $rawPath);
+        }
+
+        $public_name = basename($path);
+
+        // get the file's mime type to send the correct content type header
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime_type = finfo_file($finfo, $path);
+
+        // send the headers
+        header("Content-Disposition: attachment; filename=$public_name;");
+        header("Content-Type: $mime_type");
+        header('Content-Length: ' . filesize($path));
+
+        // stream the file
+        $fp = fopen($path, 'rb');
+        fpassthru($fp);
+        exit;
+    } catch (Exception $e) {
+        pe("error", $e);
+    }
 });
 
 $app->run();
